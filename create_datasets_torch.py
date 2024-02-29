17f63fd9acc0bbcbedb1bcee857aea866b75d522
breadbread1984 <breadbread1984@163.com>
2024-02-29 17:14:01 +0800

    update

---
+++ create_datasets_torch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+from absl import flags, app
+from shutil import rmtree
+from os import mkdir
+from os.path import join, exists
+from rdkit import Chem
+import numpy as np
+import dgl
+import torch
+import torch.nn.functional as F
+
+FLAGS = flags.FLAGS
+
+def add_options():
+  flags.DEFINE_string('input_csv', default = None, help = 'path to polymer dataset csv')
+  flags.DEFINE_string('output_dir', default = 'dataset', help = 'path to output directory')
+
+def smiles_to_sample(smiles, label):
+  molecule = Chem.MolFromSmiles(smiles)
+  indices = list()
+  nodes = list()
+  edges = list()
+  for atom in molecule.GetAtoms():
+    idx = atom.GetIdx()
+    nodes.append(atom.GetAtomicNum())
+    indices.append(idx)
+    for neighbor_atom in atom.GetNeighbors():
+      neighbor_idx = neighbor_atom.GetIdx()
+      bond = molecule.GetBondBetweenAtoms(idx, neighbor_idx)
+      edges.append((idx, neighbor_idx, bond.GetBondType()))
+  sidx = tf.argsort(indices)
+  nodes = tf.stack(nodes, axis = 0) # nodes.shape = (node_num,)
+  nodes = tf.gather(nodes, sidx)
+  edges = tf.stack(edges, axis = 0) # edges.shape = (edge_num, 3)
+  graph = dgl.heterograph({
+    ("atom", "bond", "atom"): (torch.from_numpy(edges[:,0]), torch.from_numpy(edges[:,1])),
+  })
+  graph.nodes['atom'].data['hidden'] = F.one_hot(torch.from_numpy(nodes), 118)
+  graph.edges['bond'].data['hidden'] = F.one_hot(torch.from_numpy(edges[:,2]), 22)
