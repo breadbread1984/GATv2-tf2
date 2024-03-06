aa7961d72838fd24b341e853b1f45a7868667e35
breadbread1984 <breadbread1984@163.com>
2024-03-06 15:22:04 +0800

    update

--- models.py
+++ models.py
@@ -28,7 +28,7 @@ class GATv2Convolution(tf.keras.layers.Layer):
     attention = tfgnn.softmax(graph, per_tag = tfgnn.TARGET, edge_set_name = edge_set_name, feature_value = e) # e.shape = (edge_num, head, 1)
     hi = tf.reshape(hi, (-1, self.head, self.out_channel)) # hi.shape = (edge_num, head, channel)
     print(hi.shape, attention.shape)
-    hi = hi * attention
+    hi = hi * tf.tile(attention, (1,tf.shape(hi)[1],1))
     hi = tf.reshape(hi, (-1, self.head * self.out_channel)) # hi.shape = (edge_num, head * channel)
     h = tfgnn.pool_edges_to_node(graph, edge_set_name, tfgnn.TARGET, reduce_type = 'sum', feature_value = hi) # h.shape = (node_num, head * channel)
     return h
